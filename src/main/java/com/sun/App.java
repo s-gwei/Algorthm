41f7232c5397ddd344afb746f958638b849cac78
TDsungw <guangwei.sun@china.talkdesk.com>
2021-10-29 10:47:33 +0800

    change

---
+++ src/main/java/com/sun/App.java
@@ -0,0 +1,15 @@
+package com.sun;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+    }
+}
